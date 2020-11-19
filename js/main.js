7ebd3ec7cd96d80a3b061ded9b05d2edeeb0b583
kstanczak <stanczakkrzysiek@gmail.com>
2020-11-19 17:26:11 +0100

    1

---
+++ js/main.js
@@ -0,0 +1,8 @@
+let firstNumber = 2
+let secondNumber = 4
+function add(firstNumber, secondNumber) {
+    let result = firstNumber + secondNumber
+    return result
+}
+let suma=add(firstNumber, secondNumber);
+console.log(suma);
