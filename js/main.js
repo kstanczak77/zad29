d50c9e3f6b7c0f3e6f558779f24e68a598f7b5ef
kstanczak77 <kstanczak77@users.noreply.github.com>
2020-11-22 10:20:31 +0100

    drugie

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 let firstNumber = 2
 let secondNumber = 4
-function add(firstNumber, secondNumber) {
-    let result = firstNumber + secondNumber
+function add(num1, num2) {
+    let result = num1 + num2
     return result
 }
 let suma=add(firstNumber, secondNumber);
